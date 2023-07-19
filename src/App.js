c39ae6313387fd99212a9ce05bd4b526855ec346
koraykoksal <kksl.koray@gmail.com>
2023-07-19 20:47:50 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Instructors from "./pages/Instructors";
+import NotFound from "./pages/NotFound";
+import BrowserRouter, { Route, Routes } from "react-router"
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/instructors" element={<Instructors />}/>
+            <Route path="/contact" element={<Contact />}/>
+            <Route path="*" element={<NotFound />}/>
+          </Routes>
+        <NotFound />
+        <Footer />
+    </BrowserRouter>
+
+    
+  );
+}
+
+export default App;
